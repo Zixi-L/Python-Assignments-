94bd6bb7d1d622e6ad9437cfeb3f8555ad3d7b8f
laibaobaby <49736523+laibaobaby@users.noreply.github.com>
2019-12-02 16:58:08 -0800

    Update termproject-mazeGraphics.py

--- Data Structures - Term Proj/termproject-mazeGraphics.py
+++ Data Structures - Term Proj/termproject-mazeGraphics.py
@@ -1,7 +1,4 @@
-# De Anza College, CIS 22C, Summer 2019
-# Instructor: Kamren Eftekhari
 # Term Project: Maze
-# Team Python: Michael Fan, Cameron Gao, Afshar Kiana, Zixi Luo
 # Detail: This file will provide graphical representation of the maze, as per extra credit
 
 from stack import Stack
